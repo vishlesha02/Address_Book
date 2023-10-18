87990d7c4205650934f9d56e7879cdef84cdc28f
vishlesha02 <vishleshamortale18@gmail.com>
2023-10-18 17:19:43 +0530

    Added : Add New Contact

--- AddressBook/src/com/bridgelabz/AddressBookMain.java
+++ AddressBook/src/com/bridgelabz/AddressBookMain.java
@@ -1,15 +1,28 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class AddressBookMain {
-    public static void main(String[] args)
-    {
-        System.out.println("Welcome to Address Book!");
-        InputMethods obj=new InputMethods();
-        System.out.println("-----------------------------------------------");
+    static Scanner sc=new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("<<<======= ==== Welcome to Address Book! === =======>>>");
+        InputMethods obj = new InputMethods();
 
-        System.out.println(obj.createContact());
-        System.out.println("---------------------------------------------------------------------------------------------------------------------------");
+        String s;
+            do {
+                System.out.println("Please Enter a Number for Following Options :\n 1) To Add New Contact ");
+                int option = sc.nextInt();
+                switch (option) {
+                    case 1:
+                        System.out.println(obj.createContact());
+                        System.out.println("---------------------------------------------------------------------------------------------------------------------------");
+                        break;
+                    default:
+                        break;
 
-
+                }
+                System.out.println("Do you want to have a look back? If Yes please press y");
+                s=sc.next();
+            }while(s.equals("y")||s.equals("Y"));
     }
 }
